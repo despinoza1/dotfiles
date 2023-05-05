84a319246c6bc89893af6946b47c30f24bd1554c
Daniel Espinoza <contact@daniel-espinoza.com>
2023-05-04 23:12:21 -0500

    nvim/metals: update version and configuration

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -4,5 +4,5 @@ return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
 	use "nvim-lua/plenary.nvim"
 	use "joshdick/onedark.vim"
-	use({'scalameta/nvim-metals', tag="v0.7.x", requires = { "nvim-lua/plenary.nvim" }})
+	use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }})
 end)
