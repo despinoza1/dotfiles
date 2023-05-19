7a2c1477f83454c6e1611947337870af11538c66
Daniel <contact@daniel-espinoza.com>
2023-05-19 00:25:32 -0500

    Update Neovim config

--- dot_config/nvim/lua/plugins.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd [[packadd packer.nvim]]
-
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use "nvim-lua/plenary.nvim"
-	use "joshdick/onedark.vim"
-	use({'nvim-telescope/telescope.nvim', tag = '0.1.1', requires = { "nvim-lua/plenary.nvim" }})
-	use({'scalameta/nvim-metals', requires = { "nvim-lua/plenary.nvim" }})
-end)
