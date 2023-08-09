2f736b8b885f299bdd995ff092362a43f2faa535
Daniel Espinoza <contact@daniel-espinoza.com>
2023-08-08 22:27:16 -0500

    nvim: format files

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -7,11 +7,11 @@ local cmd = vim.cmd
 cmd([[autocmd BufWritePre <buffer> lua vim.lsp.buf.format()]])
 
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  api.nvim_set_keymap(mode, lhs, rhs, options)
+    local options = { noremap = true }
+    if opts then
+        options = vim.tbl_extend("force", options, opts)
+    end
+    api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 -- global
 vim.opt_global.completeopt = { "menuone", "noinsert", "noselect" }
@@ -30,10 +30,10 @@ map("n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>")
 map("n", "<leader>f", "<cmd>lua vim.lsp.buf.format()<CR>")
 map("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>")
 map("n", "<leader>ws", '<cmd>lua require"metals".hover_worksheet()<CR>')
-map("n", "<leader>aa", [[<cmd>lua vim.diagnostic.setqflist()<CR>]]) -- all workspace diagnostics
+map("n", "<leader>aa", [[<cmd>lua vim.diagnostic.setqflist()<CR>]])                 -- all workspace diagnostics
 map("n", "<leader>ae", [[<cmd>lua vim.diagnostic.setqflist({severity = "E"})<CR>]]) -- all workspace errors
 map("n", "<leader>aw", [[<cmd>lua vim.diagnostic.setqflist({severity = "W"})<CR>]]) -- all workspace warnings
-map("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>") -- buffer diagnostics only
+map("n", "<leader>d", "<cmd>lua vim.diagnostic.setloclist()<CR>")                   -- buffer diagnostics only
 map("n", "[c", "<cmd>lua vim.diagnostic.goto_prev { wrap = false }<CR>")
 map("n", "]c", "<cmd>lua vim.diagnostic.goto_next { wrap = false }<CR>")
 
@@ -51,38 +51,38 @@ map("n", "<leader>dl", [[<cmd>lua require"dap".run_last()<CR>]])
 -- This is similiar to what I use
 local cmp = require("cmp")
 cmp.setup({
-  sources = {
-    { name = "nvim_lsp" },
-    { name = "vsnip" },
-  },
-  snippet = {
-    expand = function(args)
-      -- Comes from vsnip
-      vim.fn["vsnip#anonymous"](args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    -- None of this made sense to me when first looking into this since there
-    -- is no vim docs, but you can't have select = true here _unless_ you are
-    -- also using the snippet stuff. So keep in mind that if you remove
-    -- snippets you need to remove this select
-    ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    -- I use tabs... some say you should stick to ins-completion but this is just here as an example
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      else
-        fallback()
-      end
-    end,
-  }),
+    sources = {
+        { name = "nvim_lsp" },
+        { name = "vsnip" },
+    },
+    snippet = {
+        expand = function(args)
+            -- Comes from vsnip
+            vim.fn["vsnip#anonymous"](args.body)
+        end,
+    },
+    mapping = cmp.mapping.preset.insert({
+        -- None of this made sense to me when first looking into this since there
+        -- is no vim docs, but you can't have select = true here _unless_ you are
+        -- also using the snippet stuff. So keep in mind that if you remove
+        -- snippets you need to remove this select
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
+        -- I use tabs... some say you should stick to ins-completion but this is just here as an example
+        ["<Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+            else
+                fallback()
+            end
+        end,
+        ["<S-Tab>"] = function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            else
+                fallback()
+            end
+        end,
+    }),
 })
 
 ----------------------------------
@@ -90,7 +90,7 @@ cmp.setup({
 ----------------------------------
 local util = require 'lspconfig.util'
 
-require("lspconfig").dockerls.setup{}
+require("lspconfig").dockerls.setup {}
 -- require("lspconfig").texlab.setup{
 --   build = {
 --       args = { "-X", "compile", "%f", "--synctex", "--keep-logs", "--keep-intermediates" },
@@ -100,108 +100,108 @@ require("lspconfig").dockerls.setup{}
 --     },
 -- }
 require("lspconfig").jsonls.setup {
-  settings = {
-    json = {
-      schemas = require('schemastore').json.schemas(),
-      validate = { enabled = true },
+    settings = {
+        json = {
+            schemas = require('schemastore').json.schemas(),
+            validate = { enabled = true },
+        }
     }
-  }
 }
-require("lspconfig").yamlls.setup{
-  settings = {
-    redhat = { telemetry = { enabled = false } },
-      schemas = require('schemastore').yaml.schemas(),
-      validate = { enabled = true },
-  }
+require("lspconfig").yamlls.setup {
+    settings = {
+        redhat = { telemetry = { enabled = false } },
+        schemas = require('schemastore').yaml.schemas(),
+        validate = { enabled = true },
+    }
 }
-require("lspconfig").rust_analyzer.setup{}
+require("lspconfig").rust_analyzer.setup {}
 
-require("lspconfig").pyright.setup{}
-require("lspconfig").pylyzer.setup{}
-require("lspconfig").lua_ls.setup{}
-require("lspconfig").clangd.setup{}
-require("lspconfig").matlab_ls.setup{}
-require("lspconfig").sqlls.setup{}
-require("lspconfig").ruff_lsp.setup{
-  on_attach = function(client, bufnr)
-    client.server_capabilities.hoverProvider = false
-  end
+require("lspconfig").pyright.setup {}
+require("lspconfig").pylyzer.setup {}
+require("lspconfig").lua_ls.setup {}
+require("lspconfig").clangd.setup {}
+require("lspconfig").matlab_ls.setup {}
+require("lspconfig").sqlls.setup {}
+require("lspconfig").ruff_lsp.setup {
+    on_attach = function(client, bufnr)
+        client.server_capabilities.hoverProvider = false
+    end
 }
 require("lspconfig").diagnosticls.setup {
-  filetypes = { 
-      "python",
-      "sh",
-      "sql",
-  },
-  init_options = {
-    linters = {
-      shellcheck = {
-        command = "shellcheck",
-        debounce = 100,
-        args = { "--format=gcc", "-" },
-        offsetLine = 0,
-        offsetColumn = 0,
-        sourceName = "shellcheck",
-        formatLines = 1,
-        formatPattern = {
-          "^[^:]+:(\\d+):(\\d+):\\s+([^:]+):\\s+(.*)$",
-          {
-            line = 1,
-            column = 2,
-            message = 4,
-            security = 3,
-          },
-        },
-        securities = {
-            error = "error",
-            warning = "warning",
-            note = "info",
-        },
-      }, -- shellcheck
-      sqlfluff = {
-        command = "sqlfluff",
-        debounce = 100,
-        args = { "lint", "--disable-progress-bar", "--nofail", "-f", "json", "--dialect", "ansi", "-" },
-        offsetLine = 0,
-        offsetColumn = 0,
-        sourceName = "sqlfluff",
-        parseJson = {
-            errorsRoot = "[0].violations",
-
-            line = "line_no",
-            column = "line_pos",
-            message = "${description}",
-        },
-      }, -- sqlfluff
-    },
-    formatters = {
-      isort = {
-        command = "isort",
-        args = {"--quiet", "-"},
-        rootPatterns = { "pyproject.toml", ".isort.cfg" },
-      }, -- isort
-      black = {
-        command = "black",
-        args = { "--quiet", "-" },
-        rootPatterns = { "pyproject.toml" },
-     }, --black
-    },
     filetypes = {
-      sh = "shellcheck",
-      sql = "sqlfluff",
+        "python",
+        "sh",
+        "sql",
     },
-    formatFiletypes = {
-      python = { "isort", "black" },
+    init_options = {
+        linters = {
+            shellcheck = {
+                command = "shellcheck",
+                debounce = 100,
+                args = { "--format=gcc", "-" },
+                offsetLine = 0,
+                offsetColumn = 0,
+                sourceName = "shellcheck",
+                formatLines = 1,
+                formatPattern = {
+                    "^[^:]+:(\\d+):(\\d+):\\s+([^:]+):\\s+(.*)$",
+                    {
+                        line = 1,
+                        column = 2,
+                        message = 4,
+                        security = 3,
+                    },
+                },
+                securities = {
+                    error = "error",
+                    warning = "warning",
+                    note = "info",
+                },
+            }, -- shellcheck
+            sqlfluff = {
+                command = "sqlfluff",
+                debounce = 100,
+                args = { "lint", "--disable-progress-bar", "--nofail", "-f", "json", "--dialect", "ansi", "-" },
+                offsetLine = 0,
+                offsetColumn = 0,
+                sourceName = "sqlfluff",
+                parseJson = {
+                    errorsRoot = "[0].violations",
+
+                    line = "line_no",
+                    column = "line_pos",
+                    message = "${description}",
+                },
+            }, -- sqlfluff
+        },
+        formatters = {
+            isort = {
+                command = "isort",
+                args = { "--quiet", "-" },
+                rootPatterns = { "pyproject.toml", ".isort.cfg" },
+            }, -- isort
+            black = {
+                command = "black",
+                args = { "--quiet", "-" },
+                rootPatterns = { "pyproject.toml" },
+            }, --black
+        },
+        filetypes = {
+            sh = "shellcheck",
+            sql = "sqlfluff",
+        },
+        formatFiletypes = {
+            python = { "isort", "black" },
+        }
     }
-  }
 }
 
 local metals_config = require("metals").bare_config()
 
 -- Example of settings
 metals_config.settings = {
-  showImplicitArguments = true,
-  excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
+    showImplicitArguments = true,
+    excludedPackages = { "akka.actor.typed.javadsl", "com.github.swagger.akka.javadsl" },
 }
 
 -- *READ THIS*
@@ -218,39 +218,38 @@ metals_config.capabilities = require("cmp_nvim_lsp").default_capabilities()
 local dap = require("dap")
 
 dap.configurations.scala = {
-  {
-    type = "scala",
-    request = "launch",
-    name = "RunOrTest",
-    metals = {
-      runType = "runOrTestFile",
-      --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+    {
+        type = "scala",
+        request = "launch",
+        name = "RunOrTest",
+        metals = {
+            runType = "runOrTestFile",
+            --args = { "firstArg", "secondArg", "thirdArg" }, -- here just as an example
+        },
     },
-  },
-  {
-    type = "scala",
-    request = "launch",
-    name = "Test Target",
-    metals = {
-      runType = "testTarget",
+    {
+        type = "scala",
+        request = "launch",
+        name = "Test Target",
+        metals = {
+            runType = "testTarget",
+        },
     },
-  },
 }
 
 metals_config.on_attach = function(client, bufnr)
-  require("metals").setup_dap()
+    require("metals").setup_dap()
 end
 
 -- Autocmd that will actually be in charging of starting the whole thing
 local nvim_metals_group = api.nvim_create_augroup("nvim-metals", { clear = true })
 api.nvim_create_autocmd("FileType", {
-  -- NOTE: You may or may not want java included here. You will need it if you
-  -- want basic Java support but it may also conflict if you are using
-  -- something like nvim-jdtls which also works on a java filetype autocmd.
-  pattern = { "scala", "sbt", "java" },
-  callback = function()
-    require("metals").initialize_or_attach(metals_config)
-  end,
-  group = nvim_metals_group,
+    -- NOTE: You may or may not want java included here. You will need it if you
+    -- want basic Java support but it may also conflict if you are using
+    -- something like nvim-jdtls which also works on a java filetype autocmd.
+    pattern = { "scala", "sbt", "java" },
+    callback = function()
+        require("metals").initialize_or_attach(metals_config)
+    end,
+    group = nvim_metals_group,
 })
-
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -7,100 +7,103 @@ local opts = { noremap = true, silent = true }
 ----------------------------------
 cmd([[packadd packer.nvim]])
 require("packer").startup(function(use)
-  use({ "wbthomason/packer.nvim", opt = true })
-  use("nvim-lua/plenary.nvim")
+    use({ "wbthomason/packer.nvim", opt = true })
+    use("nvim-lua/plenary.nvim")
 
-  use {
-    "williamboman/mason.nvim",
-    run = ":MasonUpdate"
-  }
-
-
-  use({
-    "hrsh7th/nvim-cmp",
-    requires = {
-      { "hrsh7th/cmp-nvim-lsp" },
-      { "hrsh7th/cmp-vsnip" },
-      { "hrsh7th/vim-vsnip" },
-    },
-  })
-
-  use("tomtom/tcomment_vim")
-
-  ----------------------------------
-  -- UI ----------------------------
-  ----------------------------------
-  use("romgrk/barbar.nvim")
-  use("lewis6991/gitsigns.nvim")
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
+    use {
+        "williamboman/mason.nvim",
+        run = ":MasonUpdate"
     }
-  }
-  use {
-    "nvim-lualine/lualine.nvim",
-    requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-  }
-  use {
-    "ellisonleao/glow.nvim",
-    config = function() require("glow").setup() end
-  }
-  use {
-   "amrbashir/nvim-docs-view",
-   opt = true,
-   cmd = { "DocsViewToggle" },
-   config = function()
-       require("docs-view").setup {
-         position = "right",
-         width = 60,
-       }
-   end
-  }
-  use {
-    "akinsho/toggleterm.nvim",
-    tag = '*',
-    open_mapping = [[<C-\>t]],
-    config = function()
-        require("toggleterm").setup()
-    end
-  }
-  use {
-    "folke/which-key.nvim",
-    config = function ()
-      vim.o.timeout = true
-      vim.o.timeoutlen = 300
-      require("which-key").setup{}
-    end
-  }
-  use("seandewar/nvimesweeper")
 
-  ----------------------------------
-  -- THEMES ------------------------
-  ----------------------------------
-  use("joshdick/onedark.vim")
-  use({"catppuccin/nvim", as = "catppuccin"})
+    use({
+        "hrsh7th/nvim-cmp",
+        requires = {
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-vsnip" },
+            { "hrsh7th/vim-vsnip" },
+        },
+    })
 
-  ----------------------------------
-  -- LSP ---------------------------
-  ----------------------------------
-  use("lervag/vimtex")
-  use({
-    "scalameta/nvim-metals",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "mfussenegger/nvim-dap",
-    },
-  })
-  use("b0o/schemastore.nvim")
-  use("neovim/nvim-lspconfig")
+    use("tomtom/tcomment_vim")
 
-  use({
-     "nvim-telescope/telescope.nvim",
-    requires = {
-      "nvim-lua/plenary.nvim"
-    },
-  })
+    ----------------------------------
+    -- UI ----------------------------
+    ----------------------------------
+    use("romgrk/barbar.nvim")
+    use("lewis6991/gitsigns.nvim")
+    use {
+        'nvim-tree/nvim-tree.lua',
+        requires = {
+            'nvim-tree/nvim-web-devicons', -- optional, for file icons
+        }
+    }
+    use {
+        "nvim-lualine/lualine.nvim",
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    }
+    use {
+        "ellisonleao/glow.nvim",
+        config = function() require("glow").setup() end
+    }
+    use {
+        "amrbashir/nvim-docs-view",
+        opt = true,
+        cmd = { "DocsViewToggle" },
+        config = function()
+            require("docs-view").setup {
+                position = "right",
+                width = 60,
+            }
+        end
+    }
+    use {
+        "akinsho/toggleterm.nvim",
+        tag = '*',
+        open_mapping = [[<leader>t]],
+        config = function()
+            require("toggleterm").setup()
+        end
+    }
+    use {
+        "folke/which-key.nvim",
+        config = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+            require("which-key").setup {}
+        end
+    }
+    use("seandewar/nvimesweeper")
+    use {
+        "folke/todo-comments.nvim",
+        requires = { 'nvim-lua/plenary.nvim' }
+    }
+
+    ----------------------------------
+    -- THEMES ------------------------
+    ----------------------------------
+    use("joshdick/onedark.vim")
+    use({ "catppuccin/nvim", as = "catppuccin" })
+
+    ----------------------------------
+    -- LSP ---------------------------
+    ----------------------------------
+    use("lervag/vimtex")
+    use({
+        "scalameta/nvim-metals",
+        requires = {
+            "nvim-lua/plenary.nvim",
+            "mfussenegger/nvim-dap",
+        },
+    })
+    use("b0o/schemastore.nvim")
+    use("neovim/nvim-lspconfig")
+
+    use({
+        "nvim-telescope/telescope.nvim",
+        requires = {
+            "nvim-lua/plenary.nvim"
+        },
+    })
 end)
 
 -- nvim-telescope
@@ -118,38 +121,38 @@ map('n', '<A-c>', '<Cmd>BufferClose<CR>', opts)
 
 -- nvim-tree
 require("nvim-tree").setup {
-  view = {
-      width = 30,
-  }
+    view = {
+        width = 30,
+    }
 }
 require("nvim-web-devicons").setup {
-  color_icons = true;
-  default = true;
+    color_icons = true,
+    default = true,
 }
 
 -- nvim-gitsigns
 require("gitsigns").setup {
-  signcolumn = auto,
-  numhl = true,
-  on_attach = function()
-    local gs = package.loaded.gitsigns
+    signcolumn = auto,
+    numhl = true,
+    on_attach = function()
+        local gs = package.loaded.gitsigns
 
-    vim.wo.signcolumn = "yes"
+        vim.wo.signcolumn = "yes"
 
-    vim.keymap.set('n', '<leader>gb', gs.toggle_current_line_blame, {})
-    vim.keymap.set('n', '<leader>td', function()
-      gs.toggle_deleted()
-      gs.toggle_word_diff()
-    end, {})
-    vim.keymap.set('n', '<leader>hd', gs.diffthis, {})
-  end
+        vim.keymap.set('n', '<leader>gb', gs.toggle_current_line_blame, {})
+        vim.keymap.set('n', '<leader>td', function()
+            gs.toggle_deleted()
+            gs.toggle_word_diff()
+        end, {})
+        vim.keymap.set('n', '<leader>hd', gs.diffthis, {})
+    end
 }
 
 -- nvim-lualine
 require('lualine').setup {
-  options = {
-    theme = "catppuccin"
-  }
+    options = {
+        theme = "catppuccin"
+    }
 }
 
 -- mason.nvim
@@ -162,3 +165,15 @@ vim.g.vimtex_view_method = "zathura"
 --   command = "ls *.tex | entr -c tectonic /_ --synctex --keep-logs",
 -- }
 
+-- todo-comments
+require("todo-comments").setup()
+
+vim.keymap.set("n", "]t", function()
+    require("todo-comments").jump_next() -- {keywords = { "ERROR", "WARNING" }})
+end, { desc = "Next todo comment" })
+
+vim.keymap.set("n", "[t", function()
+    require("todo-comments").jump_prev()
+end, { desc = "Previous todo comment" })
+
+map('n', '<leader>t', '<Cmd>TodoTelescope<CR>', opts)
