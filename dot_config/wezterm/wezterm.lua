5dc2e5e1d427eca05d90cf473074ce1d27b22f52
Daniel Espinoza <contact@daniel-espinoza.com>
2023-12-17 18:48:57 -0600

    Add wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,19 @@
+local wezterm = require 'wezterm'
+
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+    config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font 'FiraCode Nerd Font Mono'
+config.font_size = 13.0
+
+config.enable_scroll_bar = true
+
+config.color_scheme = 'kanagawabones'
+
+return config
