0cf0979644f789a234fb4a56b61c028994408932
Daniel Espinoza <contact@daniel-espinoza.com>
2023-12-21 19:10:56 -0600

    Update treesitter and tmux

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -136,8 +136,30 @@ require("lazy").setup({
                 highlight = {
                     enable = true,
                 },
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true,
+                        keymaps = {
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V',  -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
             })
         end
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
     },
 
     {
