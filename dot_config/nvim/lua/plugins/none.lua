194559a0f42ba26078a8829c61b807867a482411
Daniel Espinoza <contact@daniel-espinoza.com>
2024-02-10 00:26:18 -0600

    nvim/none-ls: add lsp-format

--- dot_config/nvim/lua/plugins/none.lua
+++ dot_config/nvim/lua/plugins/none.lua
@@ -1,7 +1,7 @@
 return {
     {
         "nvimtools/none-ls.nvim",
-        requires = { "nvim-lua/plenary.nvim" },
+        requires = { "nvim-lua/plenary.nvim", "lukas-reineke/lsp-format.nvim" },
         config = function()
             local null_ls = require("null-ls")
 
@@ -11,25 +11,33 @@ return {
                     null_ls.builtins.formatting.beautysh,
                     null_ls.builtins.code_actions.shellcheck,
                     null_ls.builtins.diagnostics.shellcheck,
+
                     -- Python
                     null_ls.builtins.diagnostics.bandit,
                     -- null_ls.builtins.diagnostics.mypy,
                     null_ls.builtins.formatting.black,
                     null_ls.builtins.formatting.isort,
-                    -- JSON
-                    null_ls.builtins.formatting.jq,
+
                     -- LaTeX
                     null_ls.builtins.formatting.latexindent,
+
+                    -- JSON
+                    null_ls.builtins.formatting.jq,
                     -- YAML
                     null_ls.builtins.formatting.yamlfix,
                     -- TOML
                     null_ls.builtins.formatting.taplo,
+
                     -- Git
                     null_ls.builtins.diagnostics.commitlint,
+
                     -- Misc
                     null_ls.builtins.hover.dictionary,
                 },
+                on_attach = function(client, _)
+                    require("lsp-format").on_attach(client)
+                end,
             })
-        end
-    }
+        end,
+    },
 }
