6ef34137698746a92e072af36b8fe7920b7cdfe7
Daniel Espinoza <contact@daniel-espinoza.com>
2024-05-19 17:30:43 -0500

    nvim/llm: kill ollama process before quitting

--- dot_config/nvim/lua/plugins/misc.lua
+++ dot_config/nvim/lua/plugins/misc.lua
@@ -12,17 +12,41 @@ return {
   },
   {
     "David-Kunz/gen.nvim",
-    opts = {
-      model = "phi3",
-      host = "localhost",
-      port = "11434",
-      quit_map = "q",
-      retry_map = "<c-r>",
-      display_mode = "split",
-      show_prompt = true,
-      show_model = true,
-      no_auto_close = false,
-      debug = false,
-    },
+    config = function()
+      require("gen").setup({
+        model = "phi3",
+        host = "localhost",
+        port = "11434",
+        quit_map = "q",
+        retry_map = "<c-r>",
+        init = function(options)
+          local exist = vim.fn.filereadable("/tmp/gen.nvim.pid")
+          if exist ~= 1 then
+            pcall(
+              io.popen,
+              "/bin/sh -c 'echo $$>/tmp/gen.nvim.pid && exec ollama serve > /dev/null 2>&1' &"
+            )
+          end
+        end,
+        display_mode = "split",
+        show_prompt = true,
+        show_model = true,
+        no_auto_close = false,
+        debug = false,
+      })
+
+      vim.api.nvim_create_autocmd({ "QuitPre" }, {
+        group = vim.api.nvim_create_augroup("OllamaProcess", { clear = true }),
+        callback = function()
+          local exist = vim.fn.filereadable("/tmp/gen.nvim.pid")
+          if exist == 1 then
+            pcall(
+              io.popen,
+              "kill $(cat /tmp/gen.nvim.pid | awk '{print $1}') && rm /tmp/gen.nvim.pid"
+            )
+          end
+        end,
+      })
+    end,
   },
 }
