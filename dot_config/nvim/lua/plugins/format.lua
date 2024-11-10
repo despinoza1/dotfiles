67ed33352c7a77e2fe9a68fc044aaeac85a4fc40
Daniel Espinoza <contact@daniel-espinoza.com>
2024-11-09 20:00:56 -0600

    nvim/format: commands for enabling/disabling formatting

--- dot_config/nvim/lua/plugins/format.lua
+++ dot_config/nvim/lua/plugins/format.lua
@@ -1,3 +1,22 @@
+vim.api.nvim_create_user_command("FormatDisable", function(args)
+  if args.bang then
+    -- FormatDisable! will disable formatting just for this buffer
+    vim.b.disable_autoformat = true
+  else
+    vim.g.disable_autoformat = true
+  end
+end, {
+  desc = "Disable autoformat-on-save",
+  bang = true,
+})
+
+vim.api.nvim_create_user_command("FormatEnable", function()
+  vim.b.disable_autoformat = false
+  vim.g.disable_autoformat = false
+end, {
+  desc = "Re-enable autoformat-on-save",
+})
+
 return {
   {
     "stevearc/conform.nvim",
@@ -15,10 +34,12 @@ return {
     },
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_format = "fallback" and vim.fn.fnamemodify(vim.fn.bufname("%"), ":e") ~= "c" or "never",
-      },
+      format_on_save = function(bufnr)
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = "fallback" }
+      end,
       formatters = {
         yamlfix = {
           env = {
@@ -30,7 +51,7 @@ return {
         },
       },
       formatters_by_ft = {
-        -- c = { "clang-format" },
+        c = { "clang-format" },
         json = { "jq" },
         lua = { "stylua" },
         python = { "ruff_format", "isort" },
