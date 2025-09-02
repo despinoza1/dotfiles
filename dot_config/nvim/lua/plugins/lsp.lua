0d97cba62a2eea7d9a1ccf7570a2556b811456d2
Daniel Espinoza <contact@daniel-espinoza.com>
2025-09-01 20:49:39 -0500

    chore: updates

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -87,6 +87,26 @@ return {
     dependencies = {
       "b0o/schemastore.nvim",
       "nvim-telescope/telescope.nvim",
+      {
+        "nvim-java/nvim-java",
+        opts = {
+          java_test = {
+            enable = false,
+          },
+          java_debug_adapter = {
+            enable = false,
+          },
+          spring_boot_tools = {
+            enable = false,
+          },
+          jdk = {
+            auto_install = false,
+          },
+          notifications = {
+            dap = false,
+          },
+        },
+      },
       { "j-hui/fidget.nvim", opts = {} },
     },
     config = function()
