2f76e084cec79135a7ff2e4155d5cb2295611326
Daniel Espinoza <contact@daniel-espinoza.com>
2025-09-01 20:49:39 -0500

    nvim/plugins: updates

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -37,6 +37,7 @@ utils.keymap("i", "<C-k>", "<C-o>gk", { silent = true })
 -- Diagnostics
 utils.keymap("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open Diagnostic in Float" })
 utils.keymap("n", "<leader>cl", vim.diagnostic.setloclist, { desc = "Code List of Diagnostics" })
+vim.diagnostic.config({ virtual_text = true })
 
 -- Buffers
 utils.keymap("n", "<A-,>", ":bprevious<CR>", { silent = true })
