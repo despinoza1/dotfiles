5c386c113b82f8fc6cc4eba76692728d98e0881b
Daniel Espinoza <contact@daniel-espinoza.com>
2026-02-07 19:48:20 -0600

    feat(nvim): mapping for base64 encoding/decoding

--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -29,6 +29,8 @@ utils.map("n", "U", "<C-r>", { desc = "Undo" })
 utils.map({ "i", "n" }, "<esc>", "<cmd>noh<CR><esc>", { silent = true })
 utils.map({ "n", "x" }, "j", "v:count == 0 ? 'gj' : 'j'", { silent = true, expr = true })
 utils.map({ "n", "x" }, "k", "v:count == 0 ? 'gk' : 'k'", { silent = true, expr = true })
+utils.map({ "n", "v" }, "gh", "_", { desc = "Go to start of line" })
+utils.map({ "n", "v" }, "gl", "$", { desc = "Go to end of line" })
 
 -- Insert Mode
 utils.keymap("i", "<C-j>", "<C-o>gj", { silent = true })
@@ -61,6 +63,12 @@ utils.keymap("n", "[q", qf_prev, { desc = "Previous item Quickfix" })
 utils.keymap("n", "]Q", ":clast<CR>", { desc = "Last item Quickfix" })
 utils.keymap("n", "[Q", ":cfirst<CR>", { desc = "First item Quickfix" })
 utils.keymap("n", "<leader>qc", ":cclose<CR>", { desc = "Close Quickfix" })
+utils.map(
+  "n",
+  "g/",
+  ":vimgrep /<C-R>//j %<CR>|:cw<CR>",
+  { silent = true, desc = "Populate quickfix with search results" }
+)
 
 -- Location List
 local ll_next, ll_prev = make_repeatable("lnext", "lprevious")
@@ -79,3 +87,59 @@ utils.keymap(
   "y'<P'<O<ESC>'>o<ESC>:<C-u>'<,'>!/bin/sh<CR>",
   { desc = "Execute with Shell" }
 )
+
+vim.keymap.set("v", "<leader>yc", function()
+  local esc = vim.api.nvim_replace_termcodes("<Esc>", true, false, true)
+  vim.api.nvim_feedkeys(esc, "x", false)
+
+  local start_line = vim.fn.line("'<")
+  local end_line = vim.fn.line("'>")
+
+  vim.cmd(start_line .. "," .. end_line .. "yank")
+  vim.cmd((end_line + 1) .. "put")
+
+  vim.api.nvim_feedkeys("gv", "n", false)
+
+  vim.api.nvim_feedkeys("gc", "v", false)
+end, { noremap = true, desc = "Duplicate selection and comment original" })
+
+-- Base64
+local function base64_transform(command)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<ESC>", true, false, true), "x", false)
+
+  local spos = vim.fn.getpos("'<") -- [bufnum, lnum, col, off]
+  local epos = vim.fn.getpos("'>")
+
+  local srow, scol, erow, ecol
+  if spos[2] < epos[2] or (spos[2] == epos[2] and spos[3] <= epos[3]) then
+    srow, scol, erow, ecol = spos[2], spos[3], epos[2], epos[3]
+  else
+    srow, scol, erow, ecol = epos[2], epos[3], spos[2], spos[3]
+  end
+
+  local lines = vim.api.nvim_buf_get_text(0, srow - 1, scol - 1, erow - 1, ecol, {})
+  local text = table.concat(lines, "\n")
+
+  vim.fn.jobstart({ "sh", "-c", "echo -n " .. vim.fn.shellescape(text) .. " | " .. command }, {
+    stdout_buffered = true,
+    on_stdout = function(_, data)
+      if data and #data > 0 then
+        local result = {}
+        for _, line in ipairs(data) do
+          if line ~= "" then
+            table.insert(result, line)
+          end
+        end
+        vim.api.nvim_buf_set_text(0, srow - 1, scol - 1, erow - 1, ecol, result)
+      end
+    end,
+  })
+end
+
+vim.keymap.set("v", "<leader>be", function()
+  base64_transform("base64")
+end, { desc = "b64 encode selection" })
+
+vim.keymap.set("v", "<leader>bd", function()
+  base64_transform("base64 --decode")
+end, { desc = "b64 decode selection" })
